193d2ab3e4ee9f77d5040b10bb2652622df84264
Grahame Watt <watt.grah@gmail.com>
2021-01-29 16:20:12 -0600

    you can now hurt the player

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+class UI : MonoBehaviour
+{
+    public static UI Instance;
+    public Player Player
+    {
+        get => _player;
+        set
+        {
+            _player = value;
+            playerHealth = _player.health;
+            _player.OnHealthChange.AddListener(ChangeHealthDisplay);
+        }
+    }
+
+    private int playerHealth;
+
+    private Player _player;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    private void ChangeHealthDisplay(int newHealth)
+    {
+        playerHealth = newHealth;
+        Debug.Log($"New Player Health: {playerHealth} (Love UI)");
+    }
+    
+
+}
+
+
